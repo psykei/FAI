e9072f8b0dd34494a2d576335ca4f9d21a9538d3
Matteo Magnini <matteo.magnini@unibo.it>
2023-06-20 10:19:32 +0200

    fix: refactored main and metric

--- __main__.py
+++ __main__.py
@@ -1,14 +1,11 @@
 import numpy as np
-from dataset.tf_data_pipeline import AdultLoader, Processor
+from dataset.adult_data_pipeline import AdultLoader
 from fairness import enable_logging
 from fairness.metric import is_demographic_parity, is_disparate_impact, is_equalized_odds
 
 enable_logging()
 loader = AdultLoader()
-processor = Processor()
-dataset = loader.load()
-dataset = processor.setup(dataset)
-train, valid, test = loader.split(dataset)
+train, valid, test = loader.load_preprocessed_split()
 
 dp = is_demographic_parity(np.array(train["Sex"]), np.array(train["income"]))
 di = is_disparate_impact(np.array(train["Sex"]), np.array(train["income"]))
