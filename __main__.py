94ba823f3eb71d447620295aea58c9e737f5979b
Matteo Magnini <matteo.magnini@unibo.it>
2023-06-21 11:54:05 +0200

    feat: trying metrics with nn

--- __main__.py
+++ __main__.py
@@ -1,32 +1,32 @@
-import numpy as np
+from tensorflow.python.framework.ops import disable_eager_execution
+from tensorflow.python.keras.losses import binary_crossentropy
 from dataset.adult_data_pipeline import AdultLoader
 from fairness import enable_logging, logger
-from fairness.metric import (
-    is_demographic_parity,
-    is_disparate_impact,
-    is_equalized_odds,
-)
+from fairness.metric import is_demographic_parity
+from fairness.tf_metric import demographic_parity
+from utils import create_fully_connected_nn
+import numpy as np
 
+EPOCHS = 5000
+BATCH_SIZE = 500
+NEURONS_PER_LAYER = [100, 50]
+VERBOSE = 1
+
+disable_eager_execution()
 enable_logging()
 loader = AdultLoader()
 dataset = loader.load_preprocessed()
 train, valid, test = loader.load_preprocessed_split()
 
-male_data = dataset[dataset["Sex"] == 0]
-female_data = dataset[dataset["Sex"] == 1]
-male_loan_rate = sum(male_data.income) / male_data.shape[0]
-female_loan_rate = sum(female_data.income) / female_data.shape[0]
-logger.info(f"Male percentage is {male_data.shape[0] / dataset.shape[0] * 100:.1f}%")
-logger.info(f"Male loan rate is {male_loan_rate:.3f}")
-logger.info(f"Female percentage is {female_data.shape[0] / dataset.shape[0] * 100:.1f}%")
-logger.info(f"Female loan rate is {female_loan_rate:.3f}")
+idx = 3
+model = create_fully_connected_nn(train.shape[1] - 1, 1, NEURONS_PER_LAYER)
+x = model.layers[0].input
+custom_loss = lambda y_true, y_pred: binary_crossentropy(y_true, y_pred) #+ demographic_parity(idx, x, y_pred)
 
-
-dp = is_demographic_parity(np.array(train["Sex"]), np.array(train["income"]))
-di = is_disparate_impact(np.array(train["Sex"]), np.array(train["income"]))
-predicted = np.array(train["income"]) + np.random.randint(-1, 1, len(train["income"]))
-predicted = np.clip(predicted, 0, 1)
-eo = is_equalized_odds(np.array(train["Sex"]), np.array(train["income"]), predicted)
-logger.info(f"Demographic parity: {dp}")
-logger.info(f"Disparate impact: {di}")
-logger.info(f"Equalized odds: {eo}")
+model.compile(optimizer="adam", loss=custom_loss, metrics=["accuracy"])
+model.fit(train.iloc[:, :-1], train.iloc[:, -1], epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(valid.iloc[:, :-1], valid.iloc[:, -1]), verbose=VERBOSE)
+loss, accuracy = model.evaluate(test.iloc[:, :-1], test.iloc[:, -1], verbose=VERBOSE)
+logger.info(f"Test loss: {loss:.4f}")
+logger.info(f"Test accuracy: {accuracy:.4f}")
+predictions = np.squeeze(np.round(model.predict(test.iloc[:, :-1])))
+logger.info(f"Demographic parity: {is_demographic_parity(test.iloc[:, idx].to_numpy(), predictions)}")
