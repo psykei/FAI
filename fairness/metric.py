6610b27bca10dd76220f0c1f3c26cfaedabdd134
Matteo Magnini <matteo.magnini@unibo.it>
2023-06-21 11:53:20 +0200

    fix: metrics consistent with binary classification problems and binary protected feature

--- fairness/metric.py
+++ fairness/metric.py
@@ -9,117 +9,69 @@ def is_demographic_parity(p: np.array, y: np.array, epsilon=EPSILON) -> bool:
     """
     Demographic parity is a measure of fairness that measures if a value of a protected feature impacts the outcome of a
     prediction. In other words, it measures if the outcome is independent of the protected feature.
+    The protected feature must be binary.
+    The output must be binary.
     :param p: protected feature
     :param y: output
     :param epsilon: threshold for demographic parity
-    :return: False if demographic parity is less than epsilon, True otherwise
+    :return: True if demographic parity is less than epsilon, False otherwise
     """
-    result = True
-    output_values = np.unique(y)
-    for output_value in output_values:
-        output_indices = np.where(y == output_value)
-        probability = np.sum(y[output_indices]) / len(y)
-        protected_feature_values = np.unique(p[output_indices])
-        for protected_feature_value in protected_feature_values:
-            protected_feature_indices = np.where(p == protected_feature_value)
-            predictions = y[protected_feature_indices]
-            conditional_probability = np.sum(predictions) / len(predictions)
-            prob_abs_diff = abs(probability - conditional_probability)
-            if prob_abs_diff > epsilon:
-                result = False
-                fairness.logger.info(
-                    f"Demographic parity violated for output value {output_value} and protected feature value {protected_feature_value} with probability absolute difference {prob_abs_diff:.4f}"
-                )
-                break
-            else:
-                fairness.logger.info(
-                    f"Demographic parity satisfied for output value {output_value} and protected feature value {protected_feature_value} with probability absolute difference {prob_abs_diff:.4f}"
-                )
-    return result
+    assert len(np.unique(p)) <= 2, "Demographic parity is only defined for binary protected features"
+    parity = np.abs(np.mean(y[p == 0]) - np.mean(y)) + np.abs(np.mean(y[p == 1]) - np.mean(y))
+    fairness.logger.info(f"Demographic parity is {parity:.4f}")
+    return parity < epsilon
 
 
 def is_disparate_impact(
-    p: np.array, y: np.array, threshold: float = DISPARATE_IMPACT_THRESHOLD
+        p: np.array, y: np.array, threshold: float = DISPARATE_IMPACT_THRESHOLD
 ) -> bool:
     """
     Disparate impact is a measure of fairness that measures if a protected feature impacts the outcome of a prediction.
     It has been defined on binary classification problems as the ratio of the probability of a positive outcome given
     the protected feature to the probability of a positive outcome given the complement of the protected feature.
     If the ratio is less than a threshold (usually 0.8), then the prediction is considered to be unfair.
+    The protected feature must be binary.
+    The output must be binary.
     :param p: protected feature
     :param y: output
     :param threshold: threshold for disparate impact
     :return: True if disparate impact is less than threshold, False otherwise
     """
-    result = False
-    protected_feature_values, counts = np.unique(p, return_counts=True)
-    most_common_protected_feature_value = protected_feature_values[np.argmax(counts)]
-    minority_values = protected_feature_values[
-        protected_feature_values != most_common_protected_feature_value
-    ]
-    majority_indices = np.where(p == most_common_protected_feature_value)
-    majority_predictions = y[majority_indices]
-    majority_probability = np.sum(majority_predictions) / len(majority_predictions)
-    for value in minority_values:
-        minority_indices = np.where(p == value)
-        minority_predictions = y[minority_indices]
-        minority_probability = np.sum(minority_predictions) / len(minority_predictions)
-        ratio = minority_probability / majority_probability
-        if ratio <= threshold:
-            result = True
-            fairness.logger.info(
-                f"There is disparate impact for protected feature value {value} with ratio {ratio:.4f}"
-            )
-            break
-        else:
-            fairness.logger.info(
-                f"There is no disparate impact for protected feature value {value} with ratio {ratio:.4f}"
-            )
-    return result
+    protected_feature_values = np.unique(p)
+    assert (
+            len(protected_feature_values) <= 2
+    ), "Disparate impact is only defined for binary protected features"
+    first_impact = np.mean(y[p == 0]) / np.mean(y[p == 1])
+    assert first_impact > 0, "Cannot divide by zero"
+    impact = np.min([first_impact, 1 / first_impact])
+    fairness.logger.info(f"Disparate impact is {impact:.4f}")
+    return impact > threshold
 
 
 def is_equalized_odds(
-    p: np.array, y_true: np.array, y_pred: np.array, epsilon: float = EPSILON
+        p: np.array, y_true: np.array, y_pred: np.array, epsilon: float = EPSILON
 ) -> bool:
     """
     Equalized odds is a measure of fairness that measures if the output is independent of the protected feature given
     the label Y.
+    The protected feature must be binary.
+    The output must be binary.
     :param p: protected feature
     :param y_true: ground truth
     :param y_pred: prediction
     :param epsilon: threshold for equalized odds
     :return: True if equalized odds is satisfied, False otherwise
     """
-    result = True
-    true_output_values = np.unique(y_true)
-    assert (
-        len(true_output_values) <= 2
-    ), "Equalized odds is only defined for binary classification problems"
-    for output_value in true_output_values:
-        true_output_indices = np.where(y_true == output_value)
-        protected_feature_values = np.unique(p[true_output_indices])
-        assert (
-            len(protected_feature_values) <= 2
-        ), "Equalized odds is only defined for binary protected features"
-        conditional_probability = np.sum(y_pred[true_output_indices]) / len(
-            y_pred[true_output_indices]
-        )
-        for protected_feature_value in protected_feature_values:
-            protected_feature_indices = np.where(p == protected_feature_value)
-            indices = np.intersect1d(true_output_indices, protected_feature_indices)
-            predictions = y_pred[indices]
-            double_conditional_probability = np.sum(predictions) / len(predictions)
-            probability_abs_diff = abs(
-                conditional_probability - double_conditional_probability
-            )
-            if probability_abs_diff > epsilon:
-                result = False
-                fairness.logger.info(
-                    f"Equalized odds violated for output value {output_value} and protected feature value {protected_feature_value} with probability absolute difference {probability_abs_diff:.4f}"
-                )
-                break
-            else:
-                fairness.logger.info(
-                    f"Equalized odds satisfied for output value {output_value} and protected feature value {protected_feature_value} with probability absolute difference {probability_abs_diff:.4f}"
-                )
-    return result
+    conditional_prob_zero = np.mean(y_pred[y_true == 0])
+    conditional_prob_one = np.mean(y_pred[y_true == 1])
+    double_conditional_prob_zero_zero = np.mean(y_pred[(p == 0) & (y_true == 0)])
+    double_conditional_prob_zero_one = np.mean(y_pred[(p == 1) & (y_true == 0)])
+    double_conditional_prob_one_zero = np.mean(y_pred[(p == 0) & (y_true == 1)])
+    double_conditional_prob_one_one = np.mean(y_pred[(p == 1) & (y_true == 1)])
+    equalized_odds = np.abs(double_conditional_prob_zero_zero - conditional_prob_zero) + np.abs(
+        double_conditional_prob_zero_one - conditional_prob_zero
+    ) + np.abs(double_conditional_prob_one_zero - conditional_prob_one) + np.abs(
+        double_conditional_prob_one_one - conditional_prob_one
+    )
+    fairness.logger.info(f"Equalized odds is {equalized_odds:.4f}")
+    return equalized_odds < epsilon
