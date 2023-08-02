50bb8a7766de90d0d6e2345d6e7ceaf73c659e9c
Matteo Magnini <matteo.magnini@unibo.it>
2023-08-02 16:02:29 +0200

    fix: minors in EO tf metric

--- images/__init__.py
+++ images/__init__.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 from configuration import IDX_TO_NAME
 
@@ -61,11 +64,35 @@ def plot_fairness_comparison(data_files: list[Path], image_path: Path, fairness_
         data = data[data['acc'] >= 0.5]
         acc = data['acc']
         fairness = data[FAIRNESS_METRIC_SHORT_NAMES[fairness_metric]]
-        plt.scatter(fairness, acc, color=colors.pop(), marker=shapes.pop())
+        # plt.scatter(fairness, acc, color=colors.pop(), marker=shapes.pop())
+        step = 5
+        x = [np.mean(acc[int(i*step):int((i+1)*step)]) for i in range(int(acc.shape[0] / step))]
+        y = [np.mean(fairness[int(i*step):int((i+1)*step)]) for i in range(int(fairness.shape[0] / step))]
+        hidden_y = np.array(fairness).reshape(-1, 1)
+        color = colors.pop()
+        plt.plot(y, x, color=color, label=data_file.stem)
+
     plt.xlabel(FAIRNESS_METRIC_LONG_NAMES[FAIRNESS_METRIC_SHORT_NAMES[fairness_metric]])
     plt.ylabel('Accuracy')
     # add legend
     plt.legend(methods_name)
+
+    colors = ['#20E635', '#09B5E6', '#E63F14']
+    for data_file in data_files:
+        data = pd.read_csv(data_file)
+        # Exclude experiments with accuracy < 0.5
+        data = data[data['acc'] >= 0.5]
+        acc = data['acc']
+        fairness = data[FAIRNESS_METRIC_SHORT_NAMES[fairness_metric]]
+        # plt.scatter(fairness, acc, color=colors.pop(), marker=shapes.pop())
+        step = 5
+        x = [np.mean(acc[int(i*step):int((i+1)*step)]) for i in range(int(acc.shape[0] / step))]
+        y = [np.mean(fairness[int(i*step):int((i+1)*step)]) for i in range(int(fairness.shape[0] / step))]
+        hidden_y = np.array(fairness).reshape(-1, 1)
+        std_devs = [np.std(hidden_y[int(i*step):int((i+1)*step)]) for i in range(int(hidden_y.shape[0] / step))]
+        color = colors.pop()
+        plt.fill_between(y, np.array(x) - np.array(std_devs), np.array(x) + np.array(std_devs), color=color, alpha=0.2)
+
     # save to file
     plt.savefig(image_path / f"{fairness_metric}_{IDX_TO_NAME[idx]}.pdf")
     plt.clf()
