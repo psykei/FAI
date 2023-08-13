bc50a9e80fa5a5c07963d179d8961285d3fd3dec
Matteo Magnini <matteo.magnini@unibo.it>
2023-08-13 15:50:30 +0200

    feat: add readme

--- configuration.py
+++ configuration.py
@@ -12,10 +12,9 @@ BATCH_SIZE = 500
 NEURONS_PER_LAYER = [100, 50]
 VERBOSE = 0
 IDXS = [8, 7, 0]  # index of the sensitive attribute [0 = age, 7 = ethnicity, 8 = sex,...]
-# IDXS = [7]
 CUSTOM_METRICS = ["demographic_parity", "disparate_impact", "equalized_odds"]
-# CUSTOM_METRICS = ["demographic_parity", "disparate_impact", "equalized_odds"]
 ONE_HOT = False
+
 IDX_TO_NAME = {
     0: "age",
     7: "ethnicity",
@@ -44,12 +43,12 @@ def generate_lambdas(max_lambda: float, steps: float, min_lambda: float = 0.):
 
 # Hyperparameters of our method
 OUR_MAX_LAMBDAS_DP = [10, 100, 4]
-OUR_MIN_LAMBDAS_DP = [0, 0, 0]  # [0, 10, 0]
+OUR_MIN_LAMBDAS_DP = [0, 0, 0]
 OUR_STEPS_DP = [0.1, 1, 0.04]
 
-OUR_MAX_LAMBDAS_DI = [0.5, 0.1, 0.3]  # [1, 1, 4]
+OUR_MAX_LAMBDAS_DI = [0.5, 0.1, 0.3]
 OUR_MIN_LAMBDAS_DI = [0, 0, 0]
-OUR_STEPS_DI = [0.01, 0.001, 0.01]  # [0.1, 0.01, 0.5]
+OUR_STEPS_DI = [0.01, 0.001, 0.01]
 
 OUR_MAX_LAMBDAS_EO = [5, 3, 12]
 OUR_MIN_LAMBDAS_EO = [0, 0, 0]
@@ -65,6 +64,7 @@ def our_lambdas(index: int, metric: str = "demographic_parity"):
         result = generate_lambdas(OUR_MAX_LAMBDAS_DI[IDX_TO_IDX[index]], OUR_STEPS_DI[IDX_TO_IDX[index]], OUR_MIN_LAMBDAS_DI[IDX_TO_IDX[index]])
     else:
         raise ValueError(f"Unknown metric: {metric}")
+    # Always add 0. to the list of lambdas for the vanilla model
     return result if 0. in result else result + [0.]
 
 
@@ -74,7 +74,7 @@ CHO_H = 0.1
 CHO_DELTA = 1.0
 
 CHO_MAX_LAMBDAS_DP = [1, 1, 1]
-CHO_MIN_LAMBDAS_DP = [0, 0.99, 0]  # [0, 0.985, 0]
+CHO_MIN_LAMBDAS_DP = [0, 0.99, 0]
 CHO_STEPS_DP = [0.01, 0.0001, 0.01]
 
 CHO_MAX_LAMBDAS_EO = [1, 0.999, 1]
@@ -85,13 +85,16 @@ CHO_STEPS_EO = [0.001, 0.0001, 0.05]
 def cho_lambdas(index: int, metric: str = "demographic_parity"):
     if metric == "demographic_parity":
         if index == 7:
+            # Here we add two more intervals to have a better resolution of the trade-off curve
             return generate_lambdas(0.98, 0.02, 0.952) + generate_lambdas(0.95, 0.05, 0.5) + generate_lambdas(CHO_MAX_LAMBDAS_DP[1], CHO_STEPS_DP[1], CHO_MIN_LAMBDAS_DP[1])
         else:
             return generate_lambdas(CHO_MAX_LAMBDAS_DP[IDX_TO_IDX[index]], CHO_STEPS_DP[IDX_TO_IDX[index]], CHO_MIN_LAMBDAS_DP[IDX_TO_IDX[index]])
     elif metric == "equalized_odds":
         if index == 0:
+            # Here we add two more intervals to have a better resolution of the trade-off curve
             return generate_lambdas(CHO_MAX_LAMBDAS_EO[0], 0.0001, 0.999) + generate_lambdas(0.8, 0.05, 0) + generate_lambdas(CHO_MAX_LAMBDAS_EO[0], CHO_STEPS_EO[0], CHO_MIN_LAMBDAS_EO[0])
         if index == 8:
+            # Here we add one more interval to have a better resolution of the trade-off curve
             return generate_lambdas(CHO_MAX_LAMBDAS_EO[0], 0.001, 0.98) + generate_lambdas(CHO_MAX_LAMBDAS_EO[2], CHO_STEPS_EO[2], CHO_MIN_LAMBDAS_EO[2])
         else:
             return generate_lambdas(CHO_MAX_LAMBDAS_EO[IDX_TO_IDX[index]], CHO_STEPS_EO[IDX_TO_IDX[index]], CHO_MIN_LAMBDAS_EO[IDX_TO_IDX[index]])
@@ -100,11 +103,10 @@ def cho_lambdas(index: int, metric: str = "demographic_parity"):
 
 
 CHO_METRICS = ["demographic_parity", "equalized_odds"]
-# CHO_METRICS = ["demographic_parity", "disparate_impact", "equalized_odds"]
 
 # Jiang's method configuration
 JIANG_MAX_LAMBDA = [100, 120, 3]
-JIANG_MIN_LAMBDAS = [0, 0, 0]  # [0, 20, 0]
+JIANG_MIN_LAMBDAS = [0, 0, 0]
 JIANG_STEPS = [1, 1, 0.05]
 
 
