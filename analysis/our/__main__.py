c36984a533a06e39a8866f4664ae4c6d5bfdfe63
Matteo Magnini <matteo.magnini@unibo.it>
2024-01-06 11:51:51 +0100

    chore: DI sex updated

--- analysis/our/__main__.py
+++ analysis/our/__main__.py
@@ -23,10 +23,10 @@ for CUSTOM_METRIC in CUSTOM_METRICS:
                 path=OUR_PATH, custom_metric=CUSTOM_METRIC, l=LAMBDA, idx=IDX
             )
             for file in files:
-                # if CUSTOM_METRIC == "disparate_impact" and IDX == 7:
-                #     if os.path.isfile(file):
-                #         os.remove(file)
-                #     continue
+                #if CUSTOM_METRIC == "disparate_impact" and IDX == 8:
+                #    if os.path.isfile(file):
+                #        os.remove(file)
+                #    continue
                 acc, prec, rec, f1, dp, di, eo = get_final_metrics_from_file(file)
                 accs.append(acc)
                 precs.append(prec)
